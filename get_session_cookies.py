52d9134d3666aac4ad7418439bacbe4cc8ddb671
David H. Wei <davidwei@dunelm.org.uk>
2016-06-06 14:10:18 +0100

    CC0 license header

--- get_session_cookies.py
+++ get_session_cookies.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python
+# enjin-scraper
+# Written in 2016 by David H. Wei <https://github.com/spikeh/>
+
+# To the extent possible under law, the author(s) have dedicated all
+# copyright and related and neighboring rights to this software to the
+# public domain worldwide. This software is distributed without any
+# warranty.
+
+# You should have received a copy of the CC0 Public Domain Dedication
+# along with this software. If not, see
+# <http://creativecommons.org/publicdomain/zero/1.0/>.
 import json
 import pickle
 from selenium import webdriver
