997ed3978a2a78ac7a13d3406213e7a74b226979
David H. Wei <davidwei@dunelm.org.uk>
2016-06-06 14:04:05 +0100

    PEP8 line length

--- get_session_cookies.py
+++ get_session_cookies.py
@@ -9,14 +9,17 @@ with open('config.json', 'r') as f:
 browser = webdriver.Firefox()
 browser.get('http://essencesunstrider.enjin.com/login')
 
-inputs = browser.find_elements_by_xpath("//div[contains(@class, 'input') and contains(@class, 'input-text')]")
+in_xp = "//div[contains(@class, 'input') and contains(@class, 'input-text')]"
+inputs = browser.find_elements_by_xpath(in_xp)
 username = inputs[0].find_element_by_tag_name('input')
 password = inputs[1].find_element_by_tag_name('input')
 
 username.send_keys(config['username'])
 password.send_keys(config['password'])
 
-submit = browser.find_element_by_xpath('//*[@id="section-main"]/div/div[3]/div[2]/div[8]/table/tbody/tr/td/div/div/div/div/table/tbody/tr/td[2]/form/div[5]/div/input')
+sub_xp = ('//*[@id="section-main"]/div/div[3]/div[2]/div[8]/table/tbody/tr/td/'
+          'div/div/div/div/table/tbody/tr/td[2]/form/div[5]/div/input')
+submit = browser.find_element_by_xpath(sub_xp)
 submit.click()
 
 cookies = browser.get_cookies()
