6eb71a2ec34ea008bf8fe1a5a2c240d95bcf8cf7
David H. Wei <davidwei@dunelm.org.uk>
2016-06-09 12:02:42 +0100

    Remove explicit unicode strings; Python 3 strings are unicode by default

--- enjinuity/objects.py
+++ enjinuity/objects.py
@@ -30,45 +30,51 @@ def bbcode_formatter(element, children):
     if element.tag == 'br':
         return '\r'.rstrip()
     if element.tag == 'a':
-        return u"[url={link}]{text}[/url]".format(link=element.get('href'), text=children)
+        return "[url={link}]{text}[/url]".format(link=element.get('href'),
+                                                 text=children)
     if element.tag == 'img':
-        return u"[img={link}]{text}[/img]".format(link=element.get('src'), text=children)
+        return "[img={link}]{text}[/img]".format(link=element.get('src'),
+                                                 text=children)
     if element.tag in ['b', 'strong']:
-        return u"[b]{text}[/b]".format(text=children)
+        return "[b]{text}[/b]".format(text=children)
     if element.tag in ['em', 'i']:
-        return u"[i]{text}[/i]".format(text=children)
+        return "[i]{text}[/i]".format(text=children)
     if element.tag in ['del', 's']:
-        return u"[s]{text}[/s]".format(text=children)
+        return "[s]{text}[/s]".format(text=children)
     if element.tag == 'u':
-        return u"[u]{text}[/u]".format(text=children)
+        return "[u]{text}[/u]".format(text=children)
     if element.tag == 'title':
-        return u""
+        return ""
     if element.tag == 'span':
         if "font-size" in element.get('style'):
             size = element.get('style').split(':')
-            return u"[size={size}]{text}[/size]".format(text=children, size=size[1])    
+            return "[size={size}]{text}[/size]".format(text=children,
+                                                       size=size[1])
         elif "color" in element.get('style'):
             hexcolor = element.get('style').split('#')
-            return u"[color=#{color}]{text}[/color]".format(text=children, color=hexcolor[1])
-    if element.tag =='param' and element.get('name') == 'movie' and "youtube" in element.get('value'):
+            return "[color=#{color}]{text}[/color]".format(text=children,
+                                                           color=hexcolor[1])
+    if (element.tag =='param' and element.get('name') == 'movie' and
+            "youtube" in element.get('value')):
         firstSplit = element.get('value').split('&')
         secondSplit = firstSplit[0].split('/')
-        return u"[video=youtube]http://youtube.com/watch?v={value}[/video]".format(value=secondSplit[4])
+        return ("[video=youtube]http://youtube.com/watch?v={value}"
+                "[/video]").format(value=secondSplit[4])
     if element.tag =='ol': #numered list
-        return u"[list=1]{text}[/list]".format(text=children)
+        return "[list=1]{text}[/list]".format(text=children)
     if element.tag =='ul': #bullepoint list
-        return u"[list]{text}[/list]".format(text=children)
+        return "[list]{text}[/list]".format(text=children)
     if element.tag =='li': #list item
-        return u"[*]{text}".format(text=children)
+        return "[*]{text}".format(text=children)
     if element.tag =='strike':
-        return u"[s]{text}[/s]".format(text=children)
+        return "[s]{text}[/s]".format(text=children)
     if element.tag =='div':
         if(element.get('style') == 'text-align:center'):
-            return u"[align=center]{text}[align]".format(text=children)
+            return "[align=center]{text}[align]".format(text=children)
         elif(element.get('style') == 'text-align:left'):
-            return u"[align=left]{text}[align]".format(text=children)
+            return "[align=left]{text}[align]".format(text=children)
         elif(element.get('style') == 'text-align:right'):
-            return u"[align=right]{text}[align]".format(text=children)
+            return "[align=right]{text}[align]".format(text=children)
     if children:
         return children.rstrip()
 
